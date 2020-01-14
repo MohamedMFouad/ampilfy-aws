44ecfdca11ed89513c1280fd409b7557b2dd459e
Mohamed Fouad <mohamed.m.fouad.mahmoud@gmail.com>
2020-01-14 17:04:16 +0200

    files

---
+++ src/graphql/queries.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNote = `query GetNote($id: ID!) {
+  getNote(id: $id) {
+    id
+    name
+    description
+  }
+}
+`;
+export const listNotes = `query ListNotes(
+  $filter: ModelNoteFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      name
+      description
+    }
+    nextToken
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = `subscription OnCreateNote {
+  onCreateNote {
+    id
+    name
+    description
+  }
+}
+`;
+export const onUpdateNote = `subscription OnUpdateNote {
+  onUpdateNote {
+    id
+    name
+    description
+  }
+}
+`;
+export const onDeleteNote = `subscription OnDeleteNote {
+  onDeleteNote {
+    id
+    name
+    description
+  }
+}
+`;
